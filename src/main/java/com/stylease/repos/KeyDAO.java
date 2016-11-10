887e0b61741ada1b298df43aa9b9d3bc1db24f9f
Andyman <ajo0131@gmail.com>
2016-11-10 11:36:15 -0500

    Add to User/key entities/DAOs

--- src/main/java/com/stylease/repos/KeyDAO.java
+++ src/main/java/com/stylease/repos/KeyDAO.java
@@ -2,6 +2,7 @@ package com.stylease.repos;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.stylease.entities.Board;
@@ -31,12 +33,37 @@ public class KeyDAO extends AbstractIdDAO<Key> {
       + " INNER JOIN board b ON bk.boardid = b.id"
       + " WHERE b.id = ?";
   
+  private static final String KEY_FOR_NAME_SQL =
+      "SELECT * FROM app_key WHERE name = ?";
+  
+  private static final String ADD_KEY_TO_USER_SQL =
+      "INSERT INTO user_keys VALUES (?, ?)";
+  
+  private static final String USERID_COL = "userid";
+  private static final String KEYID_COL = "keyid";
+  
+  private static Key publicKey = null;
+  
+  private SimpleJdbcInsert userKeyAdder;
+  
   public KeyDAO() {
     super("app_key", "id");
   }
   
+  @Autowired
+  public void setDataSource(DataSource dataSource) {
+    super.setDataSource(dataSource);
+    userKeyAdder = new SimpleJdbcInsert(dataSource)
+      .withTableName("user_keys")
+      .usingColumns(USERID_COL, KEYID_COL);
+  }
+  
   public Key getForId(long id) {
     return super.getForId(id, new KeyRowMapper());
+  }
+  
+  public Key getKeyForName(String name) {
+    return this.jdbcTemplate.queryForObject(KEY_FOR_NAME_SQL, new Object[]{name}, new KeyRowMapper());
   }
   
   public List<Key> getForUser(User u) {
@@ -45,6 +72,14 @@ public class KeyDAO extends AbstractIdDAO<Key> {
   
   public List<Key> getForBoard(Board b) {
     return this.jdbcTemplate.query(KEYS_FOR_BOARD_SQL, new Object[]{b.getId()}, new KeyRowMapper());
+  }
+  
+  public void addKeyToUser(User u, Key k) {
+    HashMap<String, Object> args = new HashMap<>();
+    args.put(USERID_COL, u.getId());
+    args.put(KEYID_COL, k.getId());
+    
+    userKeyAdder.execute(args);
   }
   
   public class KeyRowMapper implements RowMapper<Key> {
@@ -62,6 +97,14 @@ public class KeyDAO extends AbstractIdDAO<Key> {
       return key;
     }
     
+  }
+  
+  public Key getPublicKey() {
+    if(publicKey == null) {
+      publicKey = getKeyForName("Public");
+    }
+    
+    return publicKey;
   }
   
 }
