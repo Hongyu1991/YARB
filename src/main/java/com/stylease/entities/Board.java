c0ff8e3aec1f0f2f4663d37db5a12c6adb831549
Andyman <ajo0131@gmail.com>
2016-11-20 05:01:18 -0500

    Implement addKey method for User & Board

--- src/main/java/com/stylease/entities/Board.java
+++ src/main/java/com/stylease/entities/Board.java
@@ -1,12 +1,15 @@
 package com.stylease.entities;
 
 import java.util.Date;
+import java.util.List;
 
 public class Board extends IdItem<Long> {
 
   private String name;
   private Date created;
   private boolean enabled;
+  private List<Message> messages;
+  private List<Key> keys;
   
   public String getName() {
     return this.name;
@@ -30,5 +33,33 @@ public class Board extends IdItem<Long> {
   
   public void setEnabled(boolean enabled) {
     this.enabled = enabled;
+  }
+  
+  public List<Message> getMessages() {
+    return this.messages;
+  }
+  
+  public void setMessages(List<Message> messages) {
+    this.messages = messages;
+  }
+  
+  public List<Key> getKeys() {
+    return this.keys;
+  }
+  
+  public void setKeys(List<Key> keys) {
+    this.keys = keys;
+  }
+  
+  public void addMessage(Message m) {
+    messages.add(m);
+  }
+  
+  public void addMessage(Message m, int i) {
+    messages.add(i, m);
+  }
+  
+  public void addKey(Key k) {
+    keys.add(k);
   }
 }
