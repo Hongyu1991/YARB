13556f2adcac67a59ed67eec16101a83ac8ba3ea
Andyman <ajo0131@gmail.com>
2016-11-21 02:07:18 -0500

    Implement BoardDAO

---
+++ src/main/java/com/stylease/repos/BoardDAO.java
@@ -0,0 +1,82 @@
+package com.stylease.repos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import com.stylease.entities.Board;
+import com.stylease.entities.Key;
+
+
+@Repository
+public class BoardDAO extends AbstractIdDAO<Board> {
+
+  @Autowired
+  private KeyDAO keyDao;
+  
+  private SimpleJdbcInsert boardAdder;
+  
+  protected BoardDAO() {
+    super("board", "id");
+  }
+  
+  @Autowired
+  public void setDataSource(DataSource dataSource) {
+    super.setDataSource(dataSource);
+    
+    boardAdder = new SimpleJdbcInsert(dataSource)
+      .withTableName("board")
+      .usingColumns("name")
+      .usingGeneratedKeyColumns("id");
+  }
+  
+  public void addBoard(Board b) {
+    HashMap<String, Object> args = new HashMap<>();
+    args.put("name", b.getName());
+    
+    Number id = boardAdder.executeAndReturnKey(args);
+    b.setId(id.longValue());
+    
+    List<Key> keys = b.getKeys();
+    Iterator<Key> keyItr = keys.iterator();
+    while(keyItr.hasNext()) {
+      keyDao.addKeyToBoard(b, keyItr.next());
+    }
+  }
+  
+  public Board getForId(long id) {
+    Board b = super.getForId(id, new BoardRowMapper());
+    if(b == null) {
+      return null;
+    }
+    
+    b.setKeys(keyDao.getForBoard(b));
+    return b;
+  }
+  
+  public class BoardRowMapper implements RowMapper<Board> {
+    
+    @Override
+    public Board mapRow(ResultSet rs, int rowNum) throws SQLException {
+      // TODO Auto-generated method stub
+      Board b = new Board();
+      
+      b.setId(rs.getLong("id"));
+      b.setName(rs.getString("name"));
+      b.setCreated(rs.getDate("created"));
+      b.setEnabled(rs.getBoolean("enabled"));
+      
+      return b;
+    }
+    
+  }
+}
