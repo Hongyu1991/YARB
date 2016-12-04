29d10f973a3d93a9fe271bb5939fa6f2597075b3
Britain Carter <bic2106@columbia.edu>
2016-12-04 15:59:33 -0500

    Attempt AJAX post of attributes: need fix

--- src/main/webapp/resources/page/js/m_list.js
+++ src/main/webapp/resources/page/js/m_list.js
@@ -10,11 +10,23 @@ var selAttrChanged = function(sel1, sel2) {
 		$('#selUserAttr option').each(function(e) {
 			if($(selected).attr('value') == $(this).attr('value')) {
 				$(this).prop('selected', true);
-				$('#txtVal').val($('#selVal option:selected').val());
+				//$('#txtVal').val($('#selVal option:selected').val());
 			}
 		});
 	}
 }
+
+function SelectHasValue(select, value) {
+    obj = document.getElementById(select);
+	console.log(obj);
+    if (obj !== null) {
+        return (obj.innerHTML.indexOf('value="' + value + '"') > -1);
+    } else {
+        return false;
+    }
+}
+
+
 
 $(function() {
 	var selAttr = document.getElementById("selAttr");
@@ -24,6 +36,7 @@ $(function() {
 	$(selVal).change(function() { selAttrChanged(selVal, selAttr); });
 	
 	$('#btnAddStyle').click(function(e) {
+		
 		
 		$('#selUserAttr option:selected').each(function() {
 			var selected = this;
@@ -54,7 +67,6 @@ $(function() {
 		$('#txtVal').val('');
 		$('#selUserAttr').focus();
 		
-		
 	});
 	
 	$('#btnRemStyle').click(function(e) {
@@ -68,5 +80,49 @@ $(function() {
 		$(selVal.options[idx]).remove();
 		
 	});
+	
+	$('#btnSend').click(function(e){
+		
+		var attrList =[];
+		var styleName=document.getElementById('txtStyleName').value;
+		
+		
+		$('#selAttr > option').each(function() {
+			attrList.push(this.value);
+			
+		});
+		
+		//Create style object with attrList and styleName attached with message
+		
+		
+		var style = {};
+		style[0]=styleName;
+		console.log(style[0]);
+		for(var i = 1; i<attrList.length; i++){
+			style[i]=attrList[i-1];
+		}
+		
+		console.log(style);
+		
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			url: "/m_list/addStyle",
+			data: JSON.stringify(style),
+			dataType: 'json',
+			 success: function (data) {
+                alert(data);
+            },
+            failure: function (errMsg) {
+            	console.log(style)
+                alert(errMsg);
+            }
+		
+		});
+		
+		
+		
+	});
+	
 	
 });
